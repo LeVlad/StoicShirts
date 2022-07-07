53a3149b5d01217861e238713fa6c219f663d8df
LeVlad <leca.vlad@gmail.com>
2022-07-07 19:46:58 +0000

    Add deployment to heroku variables

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://tbkjdbabdcnidp:72d027dccc751642f02c9f8bf8e7c0d761f039cc8d03a8f0f9b8001d5bdec651@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/daqug9935cmjj1'
+os.environ['SECRET_KEY'] = '-h#tm&3@lu@job)2*sng3%s=#+5ktq_0p9-wzd7mp83kbodo-m'
