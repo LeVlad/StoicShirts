e50a0d25b213f5605ed8360b4bd33792b8c82e72
LeVlad <leca.vlad@gmail.com>
2022-07-09 23:10:26 +0000

    Add home app and templates

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+"""A view that returns the index """
+
+def index(request):
+    return render(request, 'home/index.html')
+    
--- stoic_shirts_2022/settings.py
+++ stoic_shirts_2022/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'stoic_shirts_2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
