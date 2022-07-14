5a0aa245cac1e2435c2d7d9c9269a5957435d3f7
LeVlad <leca.vlad@gmail.com>
2022-07-14 18:39:14 +0000

    Add new heroku config database value

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ['DATABASE_URL'] = 'postgres://tbkjdbabdcnidp:72d027dccc751642f02c9f8bf8e7c0d761f039cc8d03a8f0f9b8001d5bdec651@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/daqug9935cmjj1'
+os.environ['DATABASE_URL'] = 'postgres://iftnyketygxgnc:7b04361659c03b018e515fa8633b98893a55a0f462193b35cc643adc7a9efe6b@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d9pe8ga1kuo64n'
 os.environ['SECRET_KEY'] = '-h#tm&3@lu@job)2*sng3%s=#+5ktq_0p9-wzd7mp83kbodo-m'
