1497aabc89dd9d72bcbe78e4c19dfb4c7692b266
LeVlad <leca.vlad@gmail.com>
2022-07-31 21:55:44 +0000

    Add complete fixtures with working detaip page

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
