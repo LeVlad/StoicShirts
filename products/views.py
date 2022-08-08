ec9d17533e60453b8286416956bf52edcdeaa8dd
LeVlad <leca.vlad@gmail.com>
2022-08-08 01:37:43 +0000

    Add sorting update

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Philosopher
 
 
@@ -24,15 +25,14 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category__name'    
+                sortkey = 'category__name'
 
             if "direction" in request.GET:
                 direction = request.GET['direction']
                 if "direction" == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey) 
-             
-
+ 
         if "category" in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -53,8 +53,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'        
-
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
