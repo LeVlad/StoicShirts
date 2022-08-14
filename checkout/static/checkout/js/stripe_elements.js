75324ae40941ac6ab0337c8e5b2414549dc6ef84
LeVlad <leca.vlad@gmail.com>
2022-08-14 21:19:43 +0000

    Add stripe to req.txt and confirmed checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,13 +1,14 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
+
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -20,13 +21,12 @@ var style = {
         }
     },
     invalid: {
-        color: '#ff0000',
-        iconColor: '#ff0000'
+        color: '#df00000',
+        iconColor: '#f00000'
     }
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
-
 
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ['DATABASE_URL'] = 'postgres://iftnyketygxgnc:7b04361659c03b018e515fa8633b98893a55a0f462193b35cc643adc7a9efe6b@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d9pe8ga1kuo64n'
 os.environ['SECRET_KEY'] = '-h#tm&3@lu@job)2*sng3%s=#+5ktq_0p9-wzd7mp83kbodo-m'
+os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51LWUwfA9a2OdEjKGRC4RlJrgEzTqABwX5EdHNr3dLCDJedDtQR0a5NJX0nZPDAxzXaG0Vui743mNN3xQY1wXskcs00ET0ECVvx'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51LWUwfA9a2OdEjKG186ZlWjl6IadNTbVe8tN2ckGnVosoLRx4igtkVbSIUfWYMm2O4cCwxo9CFvk5Sy0wQGlg6Qq00LmVYVDPn'
