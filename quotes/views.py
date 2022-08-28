a74432395aa96ade4d67fa385d2de89f00731fc2
LeVlad <leca.vlad@gmail.com>
2022-08-28 10:00:00 +0000

    Add quotes template and urls

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from.models import Philosopher
 
-# Create your views here.
+def quotes(request):
+    quotes = None
+    philosopher = get_list_or_404(Philosopher)
+    context = {
+        'philosopher': philosopher,
+        'quotes': quotes,
+    }
+    return render(request, 'quotes/quotes.html', context)
