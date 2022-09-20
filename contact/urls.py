c695780be21ace2f70796e1cb25685a105774e3c
LeVlad <leca.vlad@gmail.com>
2022-09-20 22:32:40 +0000

    Add potential fix to contact form redirect error

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.contactView, name='contact'),
-    path('contact/success', views.successView, name='success'),
+    path('contact/success/', views.successView, name='success'),
     path('contact/subscribe/', views.subscribeView, name='subscribe'),
 ]
