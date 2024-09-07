121e9a5c75df9b218305a602007d33d13bd27350
LeVlad <88729876+LeVlad@users.noreply.github.com>
2024-09-07 19:03:38 +0000

    Added new database config for new deployment with prisma

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 import os
 
-os.environ['DATABASE_URL'] = 'postgres://iftnyketygxgnc:7b04361659c03b018e515fa8633b98893a55a0f462193b35cc643adc7a9efe6b@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d9pe8ga1kuo64n'
+os.environ['DATABASE_URL'] = 'prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZDk2OTUwYmQtOTBmNy00YTYwLWI1Y2MtZTg3ZjI3ODc2ZmE2IiwidGVuYW50X2lkIjoiMzU1YTU1NGM5YWEwNzFjMmU0YjZjZDViYmQ5YTdhYzM4NjYyNTZkMDY5MDI1ODA0MDZhNjdmNDZmYTk2MWM1ZCIsImludGVybmFsX3NlY3JldCI6IjAwMGM5NjY2LTJkNWEtNDAzMC1iNDkwLTYzNDdjZDE2MTZjOSJ9.VoSqgByros1C7pXE8-94RpAKQtf-kJ1ao9q_I5KwN1E'
+os.environ['DIRECT_URL'] = 'postgres://iftnyketygxgnc:7b04361659c03b018e515fa8633b98893a55a0f462193b35cc643adc7a9efe6b@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d9pe8ga1kuo64n'
 os.environ['SECRET_KEY'] = '-h#tm&3@lu@job)2*sng3%s=#+5ktq_0p9-wzd7mp83kbodo-m'
 os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51LWUwfA9a2OdEjKGRC4RlJrgEzTqABwX5EdHNr3dLCDJedDtQR0a5NJX0nZPDAxzXaG0Vui743mNN3xQY1wXskcs00ET0ECVvx'
 os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51LWUwfA9a2OdEjKG186ZlWjl6IadNTbVe8tN2ckGnVosoLRx4igtkVbSIUfWYMm2O4cCwxo9CFvk5Sy0wQGlg6Qq00LmVYVDPn'
