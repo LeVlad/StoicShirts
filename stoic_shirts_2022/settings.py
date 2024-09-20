41d32bf42fc8c9e1b7bd935ac9d8bc3a0f8c8983
LeVlad <88729876+LeVlad@users.noreply.github.com>
2024-09-20 18:01:44 +0000

    Corrected render deployment host

--- stoic_shirts_2022/settings.py
+++ stoic_shirts_2022/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stoic-shirts-22.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['https://stoicshirts.onrender.com', 'localhost']
 
 
 # Application definition
