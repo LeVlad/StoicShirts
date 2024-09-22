faa264040e2a48e12bb57e29c70939010f2d84bb
LeVlad <88729876+LeVlad@users.noreply.github.com>
2024-09-22 08:24:36 +0000

    Corrected several issue with older commits

--- env.py
+++ env.py
@@ -5,3 +5,4 @@ os.environ['SECRET_KEY'] = '-h#tm&3@lu@job)2*sng3%s=#+5ktq_0p9-wzd7mp83kbodo-m'
 os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51LWUwfA9a2OdEjKGRC4RlJrgEzTqABwX5EdHNr3dLCDJedDtQR0a5NJX0nZPDAxzXaG0Vui743mNN3xQY1wXskcs00ET0ECVvx'
 os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51LWUwfA9a2OdEjKG186ZlWjl6IadNTbVe8tN2ckGnVosoLRx4igtkVbSIUfWYMm2O4cCwxo9CFvk5Sy0wQGlg6Qq00LmVYVDPn'
 os.environ['STRIPE_WH_SECRET'] = 'whsec_Y7wujbch3ZxjioSfEzoHSqvQ6e9kz1Tw'
+
